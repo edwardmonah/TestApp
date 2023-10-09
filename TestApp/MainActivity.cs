fa8666c9abb5d599d5ee5edafd3ff483a35d3058
brian <brian@LAPTOP-M2USG0O8>
2023-10-09 23:52:31 +0100

    Add project files.

---
+++ TestApp/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Android.Widget;
+
+namespace TestApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView textNumber;
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textNumber = FindViewById<TextView>(Resource.Id.textView1);
+
+            FindViewById<Button>(Resource.Id.buttonIncrement).Click += (o, e) =>
+            textNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.buttonDecrement).Click += (o, e) =>
+            textNumber.Text = (--number).ToString();
+
+        }
+
+ 
+	}
+}
